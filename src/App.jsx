8827acac0f53081021058cefe8ed0e643d995eee
Kunal Gupta <kunal22.10.2002@gmail.com>
2023-10-18 10:33:50 +0530

    design the responsive UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,44 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
-      
-    </>
-  )
+    return (
+        <>
+            <div className="container flex">
+                <div className="wrapper">
+                    <div className="row">
+                        <div className="wrap">
+                            <input type="text" name="password" id="password" readOnly/>
+                            <button className="copy">Copy</button>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <div className="col flex">
+                            <label htmlFor="length">Length</label>
+                            <input type="range" name="length" id="length" min={7} max={20} />
+                        </div>
+                        <div className="col">
+                            <input
+                                type="checkbox"
+                                name="allowNow"
+                                id="allowNum"
+                            />
+                            <label htmlFor="allowNum">Numbers</label>
+                        </div>
+                        <div className="col">
+                            <input
+                                type="checkbox"
+                                name="allowChar"
+                                id="allowChar"
+                            />
+                            <label htmlFor="allowChar">
+                                Special Characters
+                            </label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
